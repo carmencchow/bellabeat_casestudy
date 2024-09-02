06f95f797928f01de05ba51f4e10bfb3b71904c3
Carmen Chow <carmendevelops@gmail.com>
2024-09-02 11:31:00 -0400

    initial commit

---
+++ data_analysis.R
@@ -0,0 +1,19 @@
+# install packages
+install.packages("tidyverse")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("tidyr")
+install.packages("janitor")
+install.packages("readr")
+install.packages("lubridate")
+
+# run packages
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(janitor)
+library(readr)
+library(lubridate)
+
+#Import data
